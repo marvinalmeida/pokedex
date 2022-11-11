567702a8ad992930bb1712a17341a5113ee77808
marvinalmeida <marvin.almeida31@gmail.com>
2022-11-11 11:30:25 -0300

    iteração de lista de pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,34 @@
-console.log('aqui');
+const offset = 0
+const limit = 10
+
+const API_URL = ` https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonToLi (pokemon) {
+    return `  
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+                    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="${pokemon.name}"/>
+                </div>
+            </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+console.log(pokemonList);
+
+fetch(API_URL)
+.then((response)=>response.json())
+.then((jsonBody)=>jsonBody.results)
+.then((pokemons)=>{
+    for(let i = 0; i < pokemons.length; i++){
+        const pokemon = pokemons[i]
+        pokemonList.innerHTML += convertPokemonToLi(pokemon)
+    }
+}).catch((e)=>console.error(e))
